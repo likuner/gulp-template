d95779bf6cd7b83cbfcc7c9fdaf40df9e0fcd982
Hunter <273676636@qq.com>
2022-05-02 18:40:24 +0800

    config

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,57 @@ const gulp = require('gulp')
 const less = require('gulp-less')
 const cleanCss = require('gulp-clean-css')
 const autoprefixer = require('gulp-autoprefixer')
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
+const htmlmin = require('gulp-htmlmin')
+const del = require('del')
 
-gulp.task('less', function() {
-  return gulp.src('./src/styles/*.less')
+const lessTask = () => {
+  return gulp
+    .src('./src/styles/*.less')
     .pipe(less())
     .pipe(autoprefixer())
     .pipe(cleanCss())
     .pipe(gulp.dest('./dist/styles'))
-})
+}
 
-gulp.task('default', gulp.parallel('less'))
+const jsTask = () => {
+  return gulp
+    .src('./src/js/*.js')
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+}
+
+const htmlTask = () => {
+  return gulp
+    .src('./src/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      minifyCSS: true,
+      minifyJS: true
+    }))
+    .pipe(gulp.dest('./dist'))
+}
+
+const assetTask = () => {
+  return gulp
+    .src('./src/assets/*')
+    .pipe(gulp.dest('./dist/assets'))
+}
+
+const delTask = () => {
+  return del(['./dist'])
+}
+
+gulp.task('default', gulp.series(
+  delTask,
+  gulp.parallel(
+    lessTask,
+    jsTask,
+    htmlTask,
+    assetTask
+  )
+))
---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+export function hello () {
+  console.log('hello')
+}
+
+export async function fn() {
+  return 'async'
+}
